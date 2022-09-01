6f15ff565b972c8eabd69b80d33fb579749e75aa
BrkatiDzo <markopejic1993@gmail.com>
2022-09-01 22:18:32 +0200

    routing c-m

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,22 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  let history = useHistory();
 
   useEffect(() => {
     if (ref.current) {
-      mount(ref.current);
+      const { onParentNavigate } = mount(ref.current, {
+        onNavigate: ({ pathname: nextPathname }) => {
+          const { pathname } = history.location;
+          if (pathname !== nextPathname) {
+            history.push(nextPathname);
+          }
+        },
+      });
+      history.listen(onParentNavigate);
     }
   }, [ref]);
   return <div ref={ref}></div>;
